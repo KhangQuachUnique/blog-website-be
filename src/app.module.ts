ec4b6da9600c0888f050978dfa267c43594be3f3
KhangQuachUnique <kadfwfsfsvs@gmail.com>
2025-12-05 23:13:08 +0700

    feat(env): add example environment configuration and remove old example

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,7 +27,7 @@ import { UploadFileModule } from './files/upload-file.module';
       url: 'postgresql://postgres.nhmlmwlvvrdabyikxvzo:kadfwfsfsvs@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres',
       database: 'postgres',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      // synchronize: true,
     }),
     ConfigModule.forRoot({
       isGlobal: true,
