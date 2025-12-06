cd5347c47c2575d25e1544d9b7e1e323555e9c43
KhangQuachUnique <kadfwfsfsvs@gmail.com>
2025-12-06 20:36:53 +0700

    feat(notifications): add notification template entity and enum for notification types

--- src/notifications/entities/notification-template.entity.ts
+++ src/notifications/entities/notification-template.entity.ts
@@ -1,13 +1,18 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+import { ENotificationTemplateType } from '../enums/notification-template.enum';
+
 @Entity('notification_templates')
 export class NotificationTemplate {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ type: 'enum', enum: ENotificationTemplateType })
+  type: ENotificationTemplateType;
+
   @Column()
   title: string;
 
-  @Column()
+  @Column({ type: 'text' })
   message: string;
 }
