bde7291f345fe57a26c5eb384950c224ae450006
thienphuc <tthienphuc1612@gmail.com>
2025-12-08 14:39:03 +0700

    feat(reports): add PostResponseDto and plainToInstance import for report handling

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@nestjs/common';
 import { CreateReportDto } from './dto/create-report.dto';
 import { UpdateReportDto } from './dto/update-report.dto';
 import { Report } from './entities/report.entity';
+import { PostResponseDto } from 'src/blog-posts/dto/response/blog-post-response.dto';
+import { plainToInstance } from 'class-transformer';
 @Injectable()
 export class ReportsService {
   constructor(
