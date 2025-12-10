58049717b45fbe7d03b18ec41a0ee72b69f19639
KhangQuachUnique <kadfwfsfsvs@gmail.com>
2025-12-10 16:00:32 +0700

    refactor(user.factory): format coverImageUrl assignment for better readability

--- src/database/factories/user.factory.ts
+++ src/database/factories/user.factory.ts
@@ -16,7 +16,8 @@ export class UserFactory {
     user.phoneNumber = override.phoneNumber || faker.phone.number();
     user.bio = override.bio || faker.lorem.paragraph();
     user.avatarUrl = override.avatarUrl || faker.image.avatar();
-    user.coverImageUrl = override.coverImageUrl || faker.image.urlLoremFlickr({ category: 'nature' });
+    user.coverImageUrl =
+      override.coverImageUrl || faker.image.urlLoremFlickr({ category: 'nature' });
     user.dob = override.dob || faker.date.birthdate({ min: 18, max: 65, mode: 'age' });
     user.gender = override.gender || faker.helpers.enumValue(EGender);
     user.type = override.type || EUserRole.USER;
