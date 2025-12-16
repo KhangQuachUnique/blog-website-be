47beef236d88ddc15b03fabc810b0509d1341ece
rrnydhvv <phucbinbin2005@gmail.com>
2025-12-16 22:42:58 +0700

    feat(auth): update AuthResponseDto to use EUserRole for user role type

--- src/auth/dto/auth-response.dto.ts
+++ src/auth/dto/auth-response.dto.ts
@@ -1,9 +1,11 @@
+import { EUserRole } from '../../users/enums/role.enum';
+
 export class AuthResponseDto {
   accessToken: string;
   user: {
     id: number;
     username: string;
     email: string;
-    role?: string;
+    role: EUserRole;
   };
 }
